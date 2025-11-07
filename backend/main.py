4b1b6c467e0273fa92780bf8e13755e110803dd3
SudevOP1 <sudevdahitule06@gmail.com>
2025-11-08 00:44:40 +0530

    backend done

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-import traceback, json
+import traceback, json, time
 
-from utils.models import WSConnectionsManager
+from utils.models import WSConnectionsManager, WSConnection
 from utils.helpers import print_debug, print_log
 
 
@@ -12,15 +12,32 @@ manager = WSConnectionsManager(DEBUG)
 
 @app.get("/healthz")
 def health_check():
-    return {
-        "success": True,
-        "status": "ok",
-    }
+    try:
+        start_time = time.time()
+        return {
+            "success": True,
+            "health": {
+                "status": "ok",
+                "num_active_connections": len(manager.connections),
+                "time_required": round(time.time() - start_time, 2),
+            },
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "health": {"status": "error", "error": str(e)},
+        }
 
 
 @app.websocket("/ws")
 async def ws(websocket: WebSocket):
-    await manager.connect(websocket)
+    await websocket.accept()
+    connection = WSConnection(websocket)
+    await manager.add_connection(connection)
+    await manager.send_personal_msg(
+        websocket,
+        {"type": "output", "output": connection.terminal.get_output_queue()},
+    )
 
     try:
         while True:
@@ -28,16 +45,36 @@ async def ws(websocket: WebSocket):
             print_debug(DEBUG, f"recieved: {data}", "WEBSOCKET")
             data = json.loads(data)
 
-            if data.get("type") == "echo":
+            msg_type = data.get("type", None)
+            if not isinstance(msg_type, str):
                 await manager.send_personal_msg(
                     websocket,
-                    json.dumps(
-                        {
-                            "type": "echo_return",
-                            "msg": data.get("msg", ""),
-                        }
-                    ),
+                    {"type": "error", "error": "invalid 'type' string field"},
                 )
+                continue
+
+            if data.get("type") == "cmd":
+                cmd = data.get("cmd", None)
+                timeout = data.get("timeout", 5.0)
+                if cmd is None or not isinstance(cmd, str):
+                    await manager.send_personal_msg(
+                        websocket,
+                        {"type": "error", "error": "invalid 'cmd' string field"},
+                    )
+                    continue
+                if timeout is not None and not isinstance(timeout, float):
+                    await manager.send_personal_msg(
+                        websocket,
+                        {"type": "error", "error": "invalid 'timeout' float field"},
+                    )
+                    continue
+                else:
+                    connection = manager.get_connection(websocket)
+                    output = connection.terminal.run_cmd(cmd, timeout)
+                    await manager.send_personal_msg(
+                        websocket,
+                        {"type": "output", "output": output},
+                    )
 
     except WebSocketDisconnect:
         manager.disconnect(websocket)
--- backend/utils/models.py
+++ backend/utils/models.py
@@ -1,43 +1,58 @@
 from fastapi import WebSocket
-import winpty, re, queue, time, threading
+import winpty, re, queue, time, threading, json
 
 from .helpers import print_debug
+
+
+class WSConnection:
+
+    def __init__(self, websocket: WebSocket):
+        self.websocket = websocket
+        self.terminal = PseudoTerminal()
 
 
 class WSConnectionsManager:
 
     def __init__(self, debug: bool):
         self.debug = debug
-        self.active_connections: list[WebSocket] = []
+        self.connections: dict[WebSocket, WSConnection] = {}
         print_debug(self.debug, f"manager initialized")
 
-    async def connect(self, websocket: WebSocket) -> None:
-        await websocket.accept()
-        self.active_connections.append(websocket)
+    def get_connection(self, websocket: WebSocket) -> WSConnection | None:
+        return self.connections.get(websocket, None)
+
+    async def add_connection(self, connection: WSConnection) -> None:
+        self.connections[connection.websocket] = connection
         print_debug(
             self.debug,
-            f"1 client connected, current active clients = {len(self.active_connections)}",
+            f"1 client connected, current active clients = {len(self.connections)}",
         )
 
     async def disconnect(self, websocket: WebSocket) -> None:
-        self.active_connections.remove(websocket)
-        print_debug(
-            self.debug,
-            f"1 client disconnected, current active clients = {len(self.active_connections)}",
-        )
+        connection = self.get_connection(websocket)
+        if connection is not None:
+            connection.terminal.close()
+            print_debug(
+                self.debug,
+                f"1 client disconnected, current active clients = {len(self.connections)}",
+            )
 
     async def send_personal_msg(self, websocket: WebSocket, msg: str) -> None:
-        await websocket.send_text(msg)
+        connection = self.get_connection(websocket)
+        if connection is not None:
+            await connection.websocket.send_text(json.dumps(msg))
+            print_debug(self.debug, f"sent personal msg: {msg}", "WEBSOCKET")
 
     async def broadcast_msg(self, msg: str) -> None:
-        for connection in self.active_connections:
-            await connection.send_text(msg)
+        for connection in self.connections:
+            await connection.websocket.send_text(msg)
+            print_debug(self.debug, f"broadcasted msg: {msg}", "WEBSOCKET")
 
 
 class PseudoTerminal:
 
-    def __init__(self, shell: str = "cmd.exe"):
-        self.pty: winpty.PtyProcess = winpty.PtyProcess.spawn(shell)
+    def __init__(self, shell: str = "cmd.exe", cwd="C:\\"):
+        self.pty: winpty.PtyProcess = winpty.PtyProcess.spawn(shell, cwd)
         self.output_queue: queue.Queue = queue.Queue()
         self._stop_flag: bool = False
         self.reader_thread = threading.Thread(target=self._read_output, daemon=True)
@@ -45,7 +60,7 @@ class PseudoTerminal:
 
     def _clean_output(self, data: str) -> str:
         """remove ANSI escape codes to prevent garbage text"""
-        return re.sub(r"\x1b\[[0-9;?]*[A-Za-z]", "", data)
+        return (re.sub(r"\x1b\[[0-9;?]*[A-Za-z]", "", data)).replace("\r\n", "\n")
 
     def _read_output(self) -> None:
         """continuously keep reading pty output and store in queue"""
@@ -101,7 +116,7 @@ if __name__ == "__main__":
     terminal = PseudoTerminal()
 
     print(terminal.get_output_queue(), end="")
-    for cmd in ["dir", "echo hello"]:
+    for cmd in ["echo hello", "dir"]:
         print(terminal.run_cmd(cmd), end="")
 
     terminal.close()
