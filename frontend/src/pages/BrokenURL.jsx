f1a1c18e1a0cacd942a5193c15000111940bf36a
SudevOP1 <sudevdahitule06@gmail.com>
2025-11-07 16:17:06 +0530

    setup basic frontend

---
+++ frontend/src/pages/BrokenURL.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BrokenURL = () => {
+  return (
+    <div>
+      You've entered a broken url
+    </div>
+  )
+}
+
+export default BrokenURL
---
+++ frontend/src/pages/Terminal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Terminal = () => {
+  return (
+    <div className="bg-slate-950 text-white">
+        this is a terminal
+    </div>
+  )
+}
+
+export default Terminal
