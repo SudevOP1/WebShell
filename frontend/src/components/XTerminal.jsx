ae0dd733399f64cb6695f0e12b7b98bec432ebe2
SudevOP1 <sudevdahitule06@gmail.com>
2025-11-08 10:19:46 +0530

    frontend code formatting

--- frontend/src/components/XTerminal.jsx
+++ frontend/src/components/XTerminal.jsx
@@ -18,17 +18,22 @@ const XTerminal = () => {
     }
   };
 
+  // data received from backend
   useEffect(() => {
     ws.onmessage = (e) => {
       const data = JSON.parse(e.data);
       debugLog("receive data:", data);
 
+      // first response from backend received, now we can using terminal
       if (!firstResponseReceived) {
         firstResponseReceived = true;
       }
+
+      // received output of cmd
       if (data.type === "output") {
         let output = data.output;
 
+        // remove already typed cmd from output
         if (output.startsWith(enterPressedOnCmd)) {
           output = output.replace(enterPressedOnCmd + "\r\n", "");
         }
@@ -47,44 +52,48 @@ const XTerminal = () => {
       return;
     }
     terminal.open(terminalRef.current);
+
+    // key pressed
     terminal.onKey((e) => {
-      if (firstResponseReceived && enterPressedOnCmd.length === 0) {
-        const key = e.key;
-        const domEvent = e.domEvent;
-        debugLog("key:", key);
-        debugLog("domEvent.key:", domEvent.key);
-        debugLog("keyCode:", domEvent.keyCode);
-
-        // enter key pressed
-        if (domEvent.keyCode === 13) {
-          ws.send(
-            JSON.stringify({
-              type: "cmd",
-              cmd: currentCmd.trim(),
-            })
-          );
-          terminal.write("\r\n");
-          enterPressedOnCmd = currentCmd;
-        }
-
-        // backspace key pressed
-        else if (domEvent.keyCode === 8) {
-          if (currentCmd.length > 0) {
-            currentCmd = currentCmd.slice(0, -1);
-            terminal.write("\b \b");
-          }
-        }
-
-        // regular character pressed
-        else {
-          currentCmd += key;
-          terminal.write(key);
-        }
-
-        debugLog("currentCmd", currentCmd);
-        debugLog("enterPressedOnCmd:", enterPressedOnCmd);
+      if (!firstResponseReceived || enterPressedOnCmd.length !== 0) {
+        return;
       }
+      const key = e.key;
+      const domEvent = e.domEvent;
+      debugLog("key:", key);
+      debugLog("domEvent.key:", domEvent.key);
+      debugLog("keyCode:", domEvent.keyCode);
+
+      // pressed enter key
+      if (domEvent.keyCode === 13) {
+        ws.send(
+          JSON.stringify({
+            type: "cmd",
+            cmd: currentCmd.trim(),
+          })
+        );
+        terminal.write("\r\n"); // in terminal, \r\n = newline (for windows)
+        enterPressedOnCmd = currentCmd;
+      }
+
+      // pressed backspace key
+      else if (domEvent.keyCode === 8) {
+        if (currentCmd.length > 0) {
+          currentCmd = currentCmd.slice(0, -1);
+          terminal.write("\b \b"); // in terminal, \b \b = backspace
+        }
+      }
+
+      // pressed regular character
+      else {
+        currentCmd += key;
+        terminal.write(key);
+      }
+
+      debugLog("currentCmd", currentCmd);
+      debugLog("enterPressedOnCmd:", enterPressedOnCmd);
     });
+
     return () => {
       terminal.dispose();
     };
