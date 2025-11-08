149c267cd0806584d345ebd885a1ae44fc9fd624
SudevOP1 <sudevdahitule06@gmail.com>
2025-11-08 11:21:04 +0530

    allowed handling of allowedKeyCodes

--- frontend/src/components/XTerminal.jsx
+++ frontend/src/components/XTerminal.jsx
@@ -1,5 +1,5 @@
 import { Terminal } from "@xterm/xterm";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import "@xterm/xterm/css/xterm.css";
 
 const terminal = new Terminal();
@@ -63,13 +63,32 @@ const XTerminal = () => {
     }
     terminal.open(terminalRef.current);
 
+    // handle key pressed before xterm handles them
+    terminal.attachCustomKeyEventHandler((domEvent) => {
+      // block keys not in allowedKeyCodes
+      if (!allowedKeyCodes.includes(domEvent.keyCode)) {
+        debugLog("Blocked key:", domEvent.keyCode);
+
+        // return false to prevent xterm from handling this key
+        return false;
+      }
+
+      // return true to let xterm handle allowed keys normally
+      return true;
+    });
+
     // key pressed
     terminal.onKey((e) => {
+      const key = e.key;
+      const domEvent = e.domEvent;
+
+      // handle keys pressed only when:
+      // (first response from backend received)
+      // and (not waiting for output of previous cmd)
       if (!firstResponseReceived || enterPressedOnCmd.length !== 0) {
         return;
       }
-      const key = e.key;
-      const domEvent = e.domEvent;
+
       debugLog("key:", key);
       debugLog("domEvent.key:", domEvent.key);
       debugLog("keyCode:", domEvent.keyCode);
@@ -113,3 +132,78 @@ const XTerminal = () => {
 };
 
 export default XTerminal;
+
+let numberKeyCodes = [
+  48, // 0
+  49, // 1
+  50, // 2
+  51, // 3
+  52, // 4
+  53, // 5
+  54, // 6
+  55, // 7
+  56, // 8
+  57, // 9
+];
+
+let alphabetKeyCodes = [
+  65, // A
+  66, // B
+  67, // C
+  68, // D
+  69, // E
+  70, // F
+  71, // G
+  72, // H
+  73, // I
+  74, // J
+  75, // K
+  76, // L
+  77, // M
+  78, // N
+  79, // O
+  80, // P
+  81, // Q
+  82, // R
+  83, // S
+  84, // T
+  85, // U
+  86, // V
+  87, // W
+  88, // X
+  89, // Y
+  90, // Z
+];
+
+let punctuationKeyCodes = [
+  186, // ;
+  187, // =
+  188, // ,
+  189, // -
+  190, // .
+  191, // /
+  192, // `
+  219, // [
+  220, // \
+  221, // ]
+  222, // '
+];
+
+let arrowKeyCodes = [
+  37, // left arrow
+  38, // up arrow
+  39, // right arrow
+  40, // down arrow
+];
+
+let allowedKeyCodes = [
+  8, // backspace
+  13, // enter
+  16, // shift
+  20, // caps lock
+  32, // space
+];
+allowedKeyCodes += numberKeyCodes;
+allowedKeyCodes += alphabetKeyCodes;
+allowedKeyCodes += punctuationKeyCodes;
+// allowedKeyCodes += allowedKeyCodes;
